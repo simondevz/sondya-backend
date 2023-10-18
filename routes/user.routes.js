6d282b69113320cb0084edb316ecd293e69ea393
lordscoba <lordscoba2tm@gmail.com>
2023-10-18 12:29:15 -0700

    completed reset password

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,7 @@ import express from "express";
 const userRoutes = express.Router();
 
 userRoutes.post("/register/user", user.register);
-userRoutes.post("/login/user", user.login);
+// userRoutes.post("/login/user", user.login);
+// userRoutes.post("/login/user", user.login);
 
 export default userRoutes;
