64875385aed5c7e609315aaf1cbca17a992d5545
lordscoba <lordscoba2tm@gmail.com>
2023-11-09 15:16:59 +0100

    added image uploading ability

--- routes/admin/categories.routes.js
+++ routes/admin/categories.routes.js
@@ -1,11 +1,20 @@
+import upload from "../../config/file.js";
 import adminCategories from "../../controllers/admin/admin-categories.controllers.js";
 
 import express from "express";
 const AdminCategoriesRoutes = express.Router();
 
 // for admin categories
-AdminCategoriesRoutes.post("/admin/category/create", adminCategories.create);
-AdminCategoriesRoutes.put("/admin/category/update/:id", adminCategories.update);
+AdminCategoriesRoutes.post(
+  "/admin/category/create",
+  upload.array("image"),
+  adminCategories.create
+);
+AdminCategoriesRoutes.put(
+  "/admin/category/update/:id",
+  upload.array("image"),
+  adminCategories.update
+);
 AdminCategoriesRoutes.get("/admin/category/:id", adminCategories.getById);
 AdminCategoriesRoutes.delete("/admin/category/:id", adminCategories.delete);
 AdminCategoriesRoutes.get("/admin/categories", adminCategories.getAll);
--- routes/admin/services.routes.js
+++ routes/admin/services.routes.js
@@ -1,11 +1,20 @@
+import upload from "../../config/file.js";
 import adminServices from "../../controllers/admin/admin-services.controllers.js";
 
 import express from "express";
 const AdminServicesRoutes = express.Router();
 
 // for admin services
-AdminServicesRoutes.post("/admin/service/create", adminServices.create);
-AdminServicesRoutes.put("/admin/service/update/:id", adminServices.update);
+AdminServicesRoutes.post(
+  "/admin/service/create",
+  upload.array("image"),
+  adminServices.create
+);
+AdminServicesRoutes.put(
+  "/admin/service/update/:id",
+  upload.array("image"),
+  adminServices.update
+);
 AdminServicesRoutes.get("/admin/service/:id", adminServices.getById);
 AdminServicesRoutes.delete("/admin/service/:id", adminServices.delete);
 AdminServicesRoutes.get("/admin/services", adminServices.getAll);
