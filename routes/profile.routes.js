54f8eb528d21965b6f8b611eabd538881e095402
Peter <obidaberechukwu@gmail.com>
2023-11-18 11:12:14 +0100

    changed images to image

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -5,11 +5,7 @@ import express from "express";
 const profileRoutes = express.Router();
 
 profileRoutes.get("/profile/get/:id", profile.getbyid);
-profileRoutes.put(
-  "/profile/update/:id",
-  upload.array("images"),
-  profile.update
-);
+profileRoutes.put("/profile/update/:id", upload.array("image"), profile.update);
 profileRoutes.put("/profile/update/password/:id", profile.changePassword);
 profileRoutes.put("/profile/update/socials/:id", profile.updateSocialMedia);
 
