26e63757299354cf29d1869a6e3ea3929ef43ce4
lordscoba <lordscoba2tm@gmail.com>
2023-12-07 12:51:16 +0100

    added details routes

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -6,6 +6,7 @@ const homeRoutes = Router.Router();
 //Basic home
 homeRoutes.get("/products", homeList.getAllProducts);
 homeRoutes.get("/services", homeList.getAllServices);
-homeRoutes.get("/categories", homeList.getAllCategories);
+homeRoutes.get("/product/details/:id/:name", homeList.getProductById);
+homeRoutes.get("/service/details/:id/:name", homeList.getServiceById);
 
 export default homeRoutes;
